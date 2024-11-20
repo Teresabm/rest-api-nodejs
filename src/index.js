f20195bf1b425e8142852612f3368cc62aae9b9a
Teresa <tbarrera985>
2024-11-19 18:46:45 -0600

    new config cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import app from "./app.js";
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 
 app.listen(3000);
 console.log("Hello i'am listen");
